63b2ebbe748ce90c96839ee07adb670210fca5ce
Beatriz <103044603+beabritoliveira@users.noreply.github.com>
2024-05-18 22:53:25 -0300

    Update arvGeradMinim.py

--- arvGeradMinim.py
+++ arvGeradMinim.py
@@ -2,9 +2,11 @@ def avgMin(matriz):
   #local de partida => Linha 0 = A 
   custoMinimo = 0
   IN = [0]
+  paresOrdenados = []
   menor = float("inf")
   tamMatriz = len(matriz)
   elem = 0
+  deriv = 0
   for _ in range(tamMatriz -1):
     tamIN = len(IN)
     menor = float("inf")
@@ -15,11 +17,9 @@ def avgMin(matriz):
             if(menor > matriz[g][h]):
               menor =  matriz[g][h]
               elem = h
-    print(menor)
-    print(elem)
-    print()
+              deriv = g
     custoMinimo += menor
     IN.append(elem)
-    print(custoMinimo)
-    print()
-  return menor, IN, custoMinimo
+    paresOrdenados.append([deriv,elem])
+    
+  return IN, custoMinimo, paresOrdenados
